2e421cfdb355ea7a188ad2a4dec379c79d654321
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-12 18:55:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,25 +19,49 @@ function addToCart(item) {
 function viewCart() {
   var newcart = ""
   for (let i = 0; i < cart.length; i++) {
-    newcart = newcart + `${cart[i].itemName} at $${cart[i].itemPrice}, `
-    if (i === cart.length - 1) {
-      newcart = newcart + "and" + `${cart[i].itemName} at $${cart[i].itemPrice}, `
+    if (i === 0) {
+      newcart = newcart + `${cart[i].itemName} at $${cart[i].itemPrice}, `
     }
-  } if (cart.length > 0) {
+    else if (i < cart.length - 1) {
+      newcart = newcart + `${cart[i].itemName} at $${cart[i].itemPrice}, `
+    }
+    else if (i === cart.length - 1) {
+      newcart = newcart + "and" + ` ${cart[i].itemName} at $${cart[i].itemPrice}, `
+    }
+  }
+  if (cart.length === 0) {
+      return "Your shopping cart is empty."
+  }
+  else if (cart.length > 0) {
     return `In your cart, you have ${newcart.slice(0, -2)}.`
-  } else if (cart.length === 0) {
-    return "Your shopping cart is empty."
   }
 }
 
 function total() {
-  // write your code here
+  var itemtotal = 0
+  for (let i = 0; i < cart.length; i++) {
+    itemtotal = itemtotal + parseInt(`${cart[i].itemPrice}`)
+  }
+  return itemtotal
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var itemposition = ""
+  for (let i = 0; i < cart.length; i++) {
+    if (item === `${cart[i].itemName}`) {
+      itemposition = i
+    }
+  }
+  if (itemposition === "") {
+    return "That item is not in your cart."
+  }
+  else {
+    cart = [...cart.slice(0, itemposition), ...cart.slice(itemposition + 1)]
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (placeOrder()) {
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
