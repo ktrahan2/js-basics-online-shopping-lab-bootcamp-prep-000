4607870611dfe146d646405c0419243ebcaad643
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-12 18:54:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price =  Math.floor(Math.random() * Math.floor(100))
+  var newitem = {itemName: item, itemPrice: price}
+  cart.push(newitem)
+  return `${newitem.itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var newcart = ""
+  for (let i = 0; i < cart.length; i++) {
+    newcart = newcart + `${cart[i].itemName} at $${cart[i].itemPrice}, `
+    if (i === cart.length - 1) {
+      newcart = newcart + "and" + `${cart[i].itemName} at $${cart[i].itemPrice}, `
+    }
+  } if (cart.length > 0) {
+    return `In your cart, you have ${newcart.slice(0, -2)}.`
+  } else if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
 }
 
 function total() {
