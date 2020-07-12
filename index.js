2dd309fa73133d9c3ab7871dbf66b5b0d0529490
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-12 19:08:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,8 +63,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
-  } else {
+  } 
+  else {
       cart = []
-      return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}'
-    }
+      return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`
+  }
 }
