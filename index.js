1ffb49c5d687dc64e7ec635d4ebf754bfaa6a080
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-12 19:14:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else if (cardNumber > 0) {
-      cart = []
       return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`
+      cart = []
   }
 }
